a4e04eccc751f51a788dceba902c1e1ea6fd7ce6
Arya <31355913+aryahassibi@users.noreply.github.com>
2022-07-06 20:18:16 +0300

    Getting the color data from the server side
    Replaced the giveColor function with getColor, implemented the functionalities of the giveColor function, in the getColor funciton in index.js so that the data is not visible to the user form sketch.js
    Added a few new arrays and dictionaries to make the transition easier.
    + some minor variable renaming from snake_case to camelCase

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'))
 app.use(express.static('public'))
-app.use(express.text({ limit: '10mb' }))
+app.use(express.text({ limit: '1mb' }))
+app.use(express.json({ limit: "1mb" }))
+
 let fs = require('fs')
 let readData = fs.readFileSync('english_words.txt', 'utf8')
 let englishWordsDataBase = readData.split('\n')
 // let theWord = englishWordsDataBase[Math.floor(Math.random() * englishWordsDataBase.length)];
-let theWord = "abcdef";
+let theWord = "shades";
+let wordLength = theWord.length
+// the keys and tiles colors
+const GREEN = [3, 222, 134]
+const YELLOW = [255, 206, 99]
+const BLUE = [22, 37, 131]
+const grayKey = 210
+const gradient = 255 / (wordLength + 2);
 
 console.log(englishWordsDataBase)
 
@@ -18,6 +27,7 @@ app.post('/checkTheWord', (request, response) => {
 
 app.get('/pick', (request, response) => {
   theWord = englishWordsDataBase[Math.floor(Math.random() * englishWordsDataBase.length)];
+  wordLength = theWord.length
   console.log(theWord)
   response.json({ result: "succesful" })
 });
@@ -25,4 +35,49 @@ app.get('/pick', (request, response) => {
 app.get('/length', (request, response) => {
   console.log(theWord, theWord.length)
   response.json({ result: theWord.length })
+});
+// Sending the a json file bck to the server containing two object based on the index of the attempt and the text associated with it.
+// - The first object is an array containing the color of the latter tiles 
+// - The second object is a dictionary containing the color info for the special keys on the keyboard
+app.post('/pickColor', (request, response) => {
+  console.log(request.body)
+  let currentAttempt = request.body.currentAttempt
+  let indexOfTheAttempt = request.body.indexOfTheAttempt
+  let lastGuess = request.body.lastGuess
+  specialKeyColors = {}
+  let colors = []
+  modifiableWordCopy = theWord
+  for (let i = 0; i < wordLength; i++) {
+    if (modifiableWordCopy[i].toUpperCase() == currentAttempt[i].toUpperCase()) {
+      colors.push(GREEN)
+      modifiableWordCopy = modifiableWordCopy.slice(0, i) + '_' + modifiableWordCopy.slice(i + 1);
+      specialKeyColors[currentAttempt[i].toUpperCase()] = GREEN
+    }
+    else {
+      if (!(currentAttempt[i].toUpperCase() in specialKeyColors) && !theWord.toUpperCase().includes(currentAttempt[i].toUpperCase())) {
+        specialKeyColors[currentAttempt[i].toUpperCase()] = grayKey
+      }
+      if (lastGuess) {
+        colors.push(BLUE)
+
+      }
+      else {
+        colors.push(BLUE.concat([gradient * (indexOfTheAttempt + 1)]))
+      }
+    }
+  }
+  for (let i = 0; i < wordLength; i++) {
+    for (let k = 0; k < wordLength; k++) {
+      if (colors[i] != GREEN && modifiableWordCopy[k].toUpperCase() == currentAttempt[i].toUpperCase()) {
+        colors[i] = YELLOW
+        modifiableWordCopy = modifiableWordCopy.slice(0, k) + '_' + modifiableWordCopy.slice(k + 1);
+        if (!(currentAttempt[i].toUpperCase() in specialKeyColors)) {
+          specialKeyColors[currentAttempt[i].toUpperCase()] = YELLOW
+        }
+        break;
+      }
+    }
+  }
+  console.log({ colors: colors, specialKeyColors })
+  response.json({ colors: colors, specialKeyColors })
 });
--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,3 @@
-// python3 -m http.server
-
 async function pickTheWord() {
   const response = await fetch('/pick');
   const jsonResponse = await response.json();
@@ -44,7 +42,8 @@ let lastGuessColors = []
 
 
 
-let keys = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'], ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'], ['Z', 'X', 'C', 'V', 'B', 'N', 'M']]
+const keys = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'], ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'], ['Z', 'X', 'C', 'V', 'B', 'N', 'M']]
+const unclassifiedKeys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
 
 let longestRowLength = Math.max(keys[0].length, keys[1].length, keys[2].length)
 
@@ -70,7 +69,14 @@ let keyPressedColor = [161, 179, 207]
 let unavailableColor = [202, 214, 232]
 let aKeyIsDown = false
 let firstPress = true
-let specialKeyColors = {};
+// the color info for each keyboard key
+let keyboardKeyColors = {}
+for (const key of unclassifiedKeys) {
+  keyboardKeyColors[key] = keyDefaultColor
+}
+// the color info for the square tiles
+let previousAttemptsColors = []
+
 let wordDataBase;
 
 function preload() {
@@ -90,44 +96,29 @@ async function theWordExists(givenWord) {
   const result = await jsonResponse.result
   return result;
 }
-
-function giveColor(number_of_attempt, lastGuess = false) {
-  let colors = []
-  modif_word = theWord
-  for (let i = 0; i < word_length; i++) {
-    if (modif_word[i].toUpperCase() == attempts[number_of_attempt][i].toUpperCase()) {
-      colors.push(GREEN)
-      modif_word = modif_word.slice(0, i) + '_' + modif_word.slice(i + 1);
-      specialKeyColors[attempts[number_of_attempt][i].toUpperCase()] = GREEN
-    }
-    else {
-      if (!(attempts[number_of_attempt][i].toUpperCase() in specialKeyColors) && !theWord.toUpperCase().includes(attempts[number_of_attempt][i].toUpperCase())) {
-        specialKeyColors[attempts[number_of_attempt][i].toUpperCase()] = grayKey
-      }
-      if (lastGuess) {
-        colors.push(BLUE)
-
-      }
-      else {
-        colors.push(BLUE.concat([gradient * (number_of_attempt + 1)]))
-      }
-    }
+// getColor send the index of the attempt and the text associated with it to the server and recieves a json containing objects
+// - The first object is an array containing the color of the latter tiles 
+// - The second object is a dictionary containing the color info for the special keys on the keyboard
+async function getColor(indexOfTheAttempt, lastGuess = false) {
+  console.log(attempts[indexOfTheAttempt], attempts, indexOfTheAttempt)
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ currentAttempt: attempts[indexOfTheAttempt], indexOfTheAttempt, lastGuess })
+  };
+  const response = await fetch('/pickColor', options);
+  const jsonResponse = await response.json();
+  let specialKeyColors = jsonResponse.specialKeyColors
+  let colors = jsonResponse.colors;
+  // changing the color of some keys ont he keyboard based on the recieved data
+  for (let item in specialKeyColors) {
+    keyboardKeyColors[item] = specialKeyColors[item]
   }
-  for (let i = 0; i < word_length; i++) {
-    for (let k = 0; k < word_length; k++) {
-      if (colors[i] != GREEN && modif_word[k].toUpperCase() == attempts[number_of_attempt][i].toUpperCase()) {
-        colors[i] = YELLOW
-        modif_word = modif_word.slice(0, k) + '_' + modif_word.slice(k + 1);
-        if (!(attempts[number_of_attempt][i].toUpperCase() in specialKeyColors)) {
-          specialKeyColors[attempts[number_of_attempt][i].toUpperCase()] = YELLOW
-        }
-        break;
-      }
-    }
-  }
-  return colors
+  console.log(jsonResponse);
+  return colors;
 }
-
 
 async function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -138,7 +129,6 @@ async function setup() {
 }
 
 async function draw() {
-  console.log(word_length)
   rectMode(CENTER);
   resizeCanvas(windowWidth, windowHeight);
   background(BG);
@@ -151,7 +141,8 @@ async function draw() {
   }
 
   for (let i = 0; i < attempts[attempts.length - 1].length; i++) {
-    if (finish) {
+    // the game has finished and the colors has been fetched completely from the server
+    if (finish && lastGuessColors[i] !== undefined) {
       fill(lastGuessColors[i])
     }
     else {
@@ -168,29 +159,26 @@ async function draw() {
     textAlign(CENTER, CENTER);
     text(attempts[attempts.length - 1].toUpperCase()[i], wordSidePadding() + wordPaddingSize() * (i + 1) + square_size() * i + square_size() * 0.5, (height) / 2);
   }
+  // all of the colors has been fetched from the server
+  if (previousAttemptsColors.length == attempts.length - 1) {
+    for (let j = 0; j < attempts.length - 1; j++) {
+      let colors = previousAttemptsColors[j]
+      for (let i = 0; i < word_length; i++) {
+        fill(colors[i])
 
-  for (let j = 0; j < attempts.length - 1; j++) {
-    let colors = giveColor(j)
+        square(wordSidePadding() + wordPaddingSize() * (i + 1) + square_size() * i + square_size() * 0.5, (height) / 2 - (attempts.length - j - 1) * (square_size() + wordPaddingSize()), square_size(), 5)
 
-    for (let i = 0; i < word_length; i++) {
-      fill(colors[i])
-
-      square(wordSidePadding() + wordPaddingSize() * (i + 1) + square_size() * i + square_size() * 0.5, (height) / 2 - (attempts.length - j - 1) * (square_size() + wordPaddingSize()), square_size(), 5)
-
-      fill(LIGHT_BG)
-      textAlign(CENTER, CENTER);
-      text(attempts[j].toUpperCase()[i], wordSidePadding() + wordPaddingSize() * (i + 1) + square_size() * i + square_size() * 0.5, (height) / 2 - (attempts.length - 1 - j) * (square_size() + wordPaddingSize()));
+        fill(LIGHT_BG)
+        textAlign(CENTER, CENTER);
+        text(attempts[j].toUpperCase()[i], wordSidePadding() + wordPaddingSize() * (i + 1) + square_size() * i + square_size() * 0.5, (height) / 2 - (attempts.length - 1 - j) * (square_size() + wordPaddingSize()));
+      }
     }
   }
 
   rectMode(CORNER)
   for (let i = 0; i < keys.length; i++) {
     for (let j = 0; j < keys[i].length; j++) {
-
-      fill(keyDefaultColor)
-      if (keys[i][j] in specialKeyColors) {
-        fill(specialKeyColors[keys[i][j]])
-      }
+      fill(keyboardKeyColors[keys[i][j]])
       keyLeftSide = keyboardSidePadding(i) + j * (buttonWidth() + keyPadding())
       keyRightSide = keyLeftSide + buttonWidth()
       keyTopSide = keyboardTopPadding() + i * (buttonHeight() + keyPadding())
@@ -248,7 +236,7 @@ async function draw() {
       if (await theWordExists(attempts[attempts.length - 1])) {
         if (attempts[attempts.length - 1].toUpperCase() == theWord.toUpperCase()) {
           finish = true
-          lastGuessColors = giveColor(attempts.length - 1, true)
+          lastGuessColors = await getColor(attempts.length - 1, true)
         }
         else if (attempts.length < number_of_tries) {
           attempts.push('');
@@ -256,7 +244,7 @@ async function draw() {
         }
         else if (attempts.length == number_of_tries) {
           finish = true;
-          lastGuessColors = giveColor(attempts.length - 1, true)
+          lastGuessColors = await getColor(attempts.length - 1, true)
         }
         firstPress = false
       }
@@ -274,19 +262,24 @@ async function draw() {
 }
 
 async function keyTyped() {
-  // await wordLength();
   if (keyCode == ENTER && attempts[attempts.length - 1].length == word_length) {
     if (await theWordExists(attempts[attempts.length - 1])) {
       if (attempts[attempts.length - 1].toUpperCase() == theWord.toUpperCase()) {
         finish = true
-        lastGuessColors = giveColor(attempts.length - 1, true)
+        lastGuessColors = await getColor(attempts.length - 1, true)
       }
       else if (attempts.length < number_of_tries) {
+        // fetching the color of the square tiles from the server
+        previousAttemptsColors = []
+        for (let j = 0; j < attempts.length; j++) {
+          previousAttemptsColors.push(await getColor(j))
+        }
+        console.log("meowmeow", previousAttemptsColors)
         attempts.push('');
       }
       else if (attempts.length == number_of_tries) {
         finish = true;
-        lastGuessColors = giveColor(attempts.length - 1, true)
+        lastGuessColors = await getColor(attempts.length - 1, true)
       }
     }
     else {
