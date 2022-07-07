e5c2e3858228ae795ea1c885a2c42619b93c4ca8
Arya <31355913+aryahassibi@users.noreply.github.com>
2022-07-07 17:06:35 +0300

    Debugged the previous commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 var portNumber = process.env.PORT || 3000;
-app.listen(port_number, () => console.log('listening at', portNumber));
+app.listen(portNumber, () => console.log('listening at', portNumber));
 app.use(express.static('public'))
 app.use(express.text({ limit: '1mb' }))
 app.use(express.json({ limit: "1mb" }))
